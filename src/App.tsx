8b645d90726088416830339b82cf04bbd3c5a269
Begin Sergey <begins.1986@mail.ru>
2022-01-24 22:54:38 +0300

    add lesson 11

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const App = () => {
+    return (
+        <div className='app-wrapper'>
+            <header className='header'>
+                <img className='logo' src="https://www.svgrepo.com/show/50992/xing-logo.svg" alt="logo"/>
+            </header>
+            <nav className='nav'>
+                <div><a href="#">Profile</a></div>
+                <div><a href="#">Messages</a></div>
+                <div><a href="#">News</a></div>
+                <div><a href="#">Music</a></div>
+                <div><a href="#">Settings</a></div>
+            </nav>
+            <div className='content'>
+                <div>
+                    <img className='content_img'
+                          src="https://img.freepik.com/free-photo/landscape-of-morning-fog-and-mountains-with-hot-air-balloons-at-sunrise_335224-794.jpg?size=626&ext=jpg"
+                          alt="img"/>
+                </div>
+                <div>
+                    ava+description
+                </div>
+                <div>Post</div>
+                <div>NewPost</div>
+
+            </div>
+        </div>
+    );
 }
+
 
 export default App;
