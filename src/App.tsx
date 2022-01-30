0bc9602143f750bfb968c688964f0f7c80ede84b
Begin Sergey <begins.1986@mail.ru>
2022-01-30 23:14:52 +0300

    add lesson 19

--- src/App.tsx
+++ src/App.tsx
@@ -4,20 +4,30 @@ import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
 import Profile from "./components/Profile/Profile";
 import Dialogs from "./components/Dialogs/Dialogs";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 
 
 const App = () => {
     return (
-        <div className='app-wrapper'>
-            <Header/>
-            <div className="content-block">
-                <Navbar/>
-                <div className="content">
-                    {/*<Dialogs/>*/}
-                    <Profile/>
+        <BrowserRouter>
+            <div className='app-wrapper'>
+                <Header/>
+                <div className="content-block">
+                    <Navbar/>
+                    <div className="content">
+                        <Routes>
+                            <Route path='/profile' element={<Profile/>}/>
+                            <Route path='/dialogs' element={<Dialogs/>}/>
+                            <Route path='/news' element={<Dialogs/>}/>
+                            <Route path='/music' element={<Dialogs/>}/>
+                            <Route path='/settings' element={<Dialogs/>}/>
+                            {/*<Dialogs/>*/}
+                            {/*<Profile/>*/}
+                        </Routes>
+                    </div>
                 </div>
             </div>
-        </div>
+        </BrowserRouter>
     );
 }
 
