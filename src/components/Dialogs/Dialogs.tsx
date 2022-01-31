dabc1dd301d2a80de4e58ebc003ff163fa32fe82
Begin Sergey <begins.1986@mail.ru>
2022-01-31 23:45:13 +0300

    add lesson 21

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,9 +1,23 @@
 import React from 'react';
+import s from './Dialogs.module.css';
 
 const Dialogs = () => {
     return (
-        <div>
-           Dialogs
+        <div className={s.dialogs}>
+            <div className={s.dialogsItem}>
+                <div className={s.dialog + ' ' + s.active}>Sergey</div>
+                <div className={s.dialog}>Stassy</div>
+                <div className={s.dialog}>Angel</div>
+                <div className={s.dialog}>Zahar</div>
+                <div className={s.dialog}>Olya</div>
+                <div className={s.dialog}>Kostya</div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}>Hello</div>
+                <div className={s.message}>Hi</div>
+                <div className={s.message}>Yo</div>
+                <div className={s.message}>Super!</div>
+            </div>
         </div>
     );
 };
