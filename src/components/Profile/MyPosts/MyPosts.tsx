e84f9aa1652fbaeac45f3e5810829a7cb43622ac
Begin Sergey <begins.1986@mail.ru>
2022-02-14 23:14:04 +0300

    add lesson 31

--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -9,15 +9,22 @@ export type MyPostPropsType = {
 const MyPosts = (props: MyPostPropsType) => {
     const postItem = props.posts.map(p => <Post message={p.message} likeCount={p.likeCount}/>)
 
+    let newPostValue = React.createRef()
+
+    let addPost = () => {
+        let text=newPostValue.current.value
+        alert(text)
+    }
+
     return (
         <div>
             <div>MyPost</div>
             <div>
                 <div>
-                    <textarea></textarea>
+                    <textarea ref={newPostValue}/>
                 </div>
                 <div>
-                    <button>Add post</button>
+                    <button onClick={addPost}>Add post</button>
                 </div>
             </div>
             <div>
